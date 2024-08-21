aa56817fa36185bb121982fb2d4e3a1d60eff569
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-08-21 19:45:58 +0200

    se instala cors para hacer peticiones

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cors = require("cors");
 const { dbConnection } = require("./config/config");
 
 app.use(express.json());
+app.use(cors());
 
 dbConnection();
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
+
 const { authentication } = require("../middleware/authentication");
 
 router.get("/users",authentication, UserController.getAllUsers);
