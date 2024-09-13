e2fe717c1f5f049945c0a6d179b33dc69d4a239d
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-09-13 11:38:45 +0200

    se crea logica para contratar servicios, se empieza a configurar ruta

--- controllers/ContractController.js
+++ controllers/ContractController.js
@@ -1,2 +1,31 @@
 const Contract = require("../models/Contract");
 const Service = require("../models/Service");
+
+const ContractController = {
+  async hireService(req, res) {
+    const userId = req.user._id;
+    const { serviceId } = req.body;
+    try {
+      const service = await Service.findById(serviceId).populate("provider");
+      if (!service) {
+        return res.status(404).json({ message: "Servicio no encontrado" });
+      }
+
+      const newContract = new Contract({
+        service: service._id,
+        user: userId,
+        provider: service.provider._id,
+        starDate: new Date(),
+      });
+      await newContract.save();
+      res.status(201).json({
+        message: "Servicio contratado con exito",
+        contract: newContract,
+      });
+    } catch (error) {
+      res
+        .statsu(500)
+        .json({ message: "Error al contratar el servicio", error });
+    }
+  },
+};
